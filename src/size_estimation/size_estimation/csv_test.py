b745effb8933704a6e164303b0e62a67a6347069
enrico <enrico.mendez@outlook.es>
2024-02-16 09:40:22 -0600

    update

--- src/size_estimation/size_estimation/csv_test.py
+++ src/size_estimation/size_estimation/csv_test.py
@@ -24,9 +24,11 @@ parts = '/'.join(parts)
 print(parts)
 os.system('cd {}'.format(parts))
 
-file_name = 'test.csv'
+file_name = '/test.csv'
 
-header = ['id', 'Nombre', 'Edad', 'Ciudad']
+full_file_name = parts+file_name
+
+header = ['Id',' Original image path', 'Processed image path', 'Area in image (px2)',' Area in image (cm2)', 'Weight (g)', 'Time stamp']
 content = [
     [1, 'Ana', 25, 'Querétaro'],
     [2, 'Carlos', 30, 'Ciudad de México'],
@@ -40,9 +42,16 @@ def reader_csv(file_name):
         data = list(text_read)
     return data
 
+def new_entry(file_name,id):
+    with open(file_name, 'a') as my_file:
+        writer = csv.writer(my_file)
+        val = int(id)+1
+        writer.writerow([val,'Ana', 25, 'Querétaro'])
+
 def create_csv(file_name):
     with open (file_name, 'x') as my_file :
         writer = csv.writer(my_file)
+        
 
         writer.writerow(header)
         for line in content:
@@ -51,12 +60,16 @@ def create_csv(file_name):
 def main():
     print('going for main')
     try: 
-        content = reader_csv(file_name)
+        content = reader_csv(full_file_name)
         print('File successfully read')
-        print('Content found: \n {}'.format(content[-1][0]))
+        id = content[-1][0]
+        print('Last id found: \n {}'.format(id))
+        print('Writing a new entry')
+        new_entry(full_file_name,id)
+
     except: 
         print('Try fail going for exception')
-        create_csv(file_name)
-        print('File couldn\'t be read, a new file {} was created'.format(file_name))
+        create_csv(full_file_name)
+        print('File couldn\'t be read, a new file {} was created'.format(full_file_name))
 
 main()
---
+++ src/size_estimation/size_estimation/data_adquisition.py
@@ -0,0 +1,66 @@
+# Author: Enrico Mendez
+# Date: 13 Febrero 2024
+# Description: Node for image capture of the peppers
+
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge
+import numpy as np
+import time
+
+
+class DataAdquisition(Node):
+    def __init__(self):
+        super().__init__('Image_capture')
+        self.get_logger().info('Image_capture initialized')
+
+        # Create variables
+        self.valid_msg = False
+        self.img_msg = np.array((720, 1280, 3))
+        
+        # Define constants
+        self.bridge = CvBridge()
+        self.timer_period = 0.5
+        self.timer = self.create_timer(self.timer_period, self.image_processing)
+
+        # Create publishers
+        
+        # Create subscribers
+        self.img_sub = self.create_subscription(Image,'/oak/rgb/image_raw', self.img_callback, 10)
+        
+
+    def img_callback(self,msg):
+        try:
+            self.org_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            self.capture_time = time.localtime(time.time())
+            self.valid_msg = True
+        except:
+            self.get_logger().info("Failed to recieved image")
+    
+    def image_processing(self):
+        if not (self.valid_msg): return
+        self.get_logger().info("Ready for capture")
+        self.get_logger().info("Press enter to capture")
+        keyboard_input = input()
+        if keyboard_input == '' : self.get_logger().info("Proceding ...")
+        print("Capture time:", time.strftime("%H:%M:%S", self.capture_time))
+        print("Printing time:", time.strftime("%H:%M:%S", time.localtime(time.time())))
+        return
+        
+            
+
+
+def main(args=None):
+    # Required lines for any node
+    rclpy.init(args=args)
+    node = DataAdquisition()
+    rclpy.spin(node)
+    # Optional but good practices
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
