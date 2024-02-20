dc3af556af6eb45a5b5d24f30eeccb2ffe471e65
enrico <enrico.mendez@outlook.es>
2024-02-20 11:36:16 -0600

    Interfaces pkg created, this pkg contains the Image processing service interface

--- install/size_estimation/lib/python3.10/site-packages/size_estimation/data_adquisition.py
+++ install/size_estimation/lib/python3.10/site-packages/size_estimation/data_adquisition.py
@@ -2,6 +2,8 @@
 # Date: 13 Febrero 2024
 # Description: Node for image capture of the peppers
 
+import csv
+from ament_index_python import get_package_share_directory
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -15,23 +17,72 @@ class DataAdquisition(Node):
     def __init__(self):
         super().__init__('Image_capture')
         self.get_logger().info('Image_capture initialized')
-
-        # Create variables
-        self.valid_msg = False
-        self.img_msg = np.array((720, 1280, 3))
         
         # Define constants
+        pkg_path = self.get_pkg_path()
+        file_name = '/data.csv'
+        self.full_file_name = pkg_path + file_name
         self.bridge = CvBridge()
         self.timer_period = 0.5
-        self.timer = self.create_timer(self.timer_period, self.image_processing)
-
-        # Create publishers
+        self.timer = self.create_timer(self.timer_period, self.data_register)
         
-        # Create subscribers
-        self.img_sub = self.create_subscription(Image,'/oak/rgb/image_raw', self.img_callback, 10)
-        
+        # Create variables
+        self.id = self.check_id(self.full_file_name)
 
-    def img_callback(self,msg):
+        # Create client
+
+    def reader_csv(self, file_name):
+
+        with open (file_name, 'r+') as my_file:
+            text_read = csv.reader(my_file)
+            data = list(text_read)
+        return data
+
+    def new_entry(self, file_name,content):
+        with open(file_name, 'a') as my_file:
+            writer = csv.writer(my_file)
+            writer.writerow(content)
+
+    def create_csv(self, file_name):
+        with open (file_name, 'x') as my_file :
+            writer = csv.writer(my_file)
+            
+            header = ['Id',' Original image path', 'Processed image path', 
+                            'Area in image (px2)',' Area in image (cm2)',
+                                'Weight (g)', 'Time stamp']
+            writer.writerow(header)
+
+    def check_id(self,file_name):
+        try:
+            content = self.reader_csv(file_name)
+            current_id = content[-1][0]
+            if current_id == 'Id': id = 1 
+            else: id = int(current_id) + 1
+        except:
+            self.create_csv(file_name)
+            id = 1
+        self.get_logger().info("Id obtained: {}".format(id))
+        return id
+
+    def get_pkg_path(self,target='size_estimation'):
+        # Get exc path
+        pkg_path = get_package_share_directory(target)
+
+        # Converting to list
+        parts = pkg_path.split('/')
+
+        # Directing to the src folder
+        replace = 'install'
+        idx = parts.index(replace)
+        parts[idx] = 'src'
+        parts.remove('share')
+
+        # Converting back to string
+        path = '/'.join(parts)
+
+        return path
+
+
         try:
             self.org_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
             self.capture_time = time.localtime(time.time())
@@ -39,8 +90,7 @@ class DataAdquisition(Node):
         except:
             self.get_logger().info("Failed to recieved image")
     
-    def image_processing(self):
-        if not (self.valid_msg): return
+    def data_register(self):
         self.get_logger().info("Ready for capture")
         self.get_logger().info("Press enter to capture")
         keyboard_input = input()
